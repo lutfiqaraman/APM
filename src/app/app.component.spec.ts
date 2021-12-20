af1090d79a323d76e4ef4ae593455e64987fc3c2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-20 22:44:02 +0200

    Building product list component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'APM'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('APM');
+    expect(app.pageTitle).toEqual('APM');
   });
 
   it('should render title', () => {
