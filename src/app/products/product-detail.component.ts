59de88153d1f783336d633cbf7f486203c15dca2
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-28 10:44:58 +0200

    create the view of product details

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Details';
 
   constructor() { }
 
