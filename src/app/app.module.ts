eebe5f71735758c5e6ce0841625dbe9467030fcf
lutfiqaraman <lutfiqaraman@gmail.com>
2021-12-28 10:49:12 +0200

    create the welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import {ProductListComponent} from "./products/product-list.component";
-import {FormsModule} from "@angular/forms";
-import {ConvertToSpaces} from "./shared/convert-to-spaces.pipe";
-import {StarComponent} from "./shared/star.component";
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+
+// Project components
+import { AppComponent } from './app.component';
+import { ProductListComponent } from "./products/product-list.component";
 import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { StarComponent } from "./shared/star.component";
+
+// Custom Pipeline
+import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
   declarations: [
@@ -15,7 +19,8 @@ import { ProductDetailComponent } from './products/product-detail.component';
     ProductListComponent,
     StarComponent,
     ConvertToSpaces,
-    ProductDetailComponent
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
