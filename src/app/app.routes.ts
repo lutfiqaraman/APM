7aae2350923223cc818fcf19222cb12174bdea0c
lutfiqaraman <lutfiqaraman@gmail.com>
2022-01-03 21:55:50 +0200

    Build a product guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from "@angular/router";
 import {ProductListComponent} from "./products/ProductsList/product-list.component";
 import {ProductDetailComponent} from "./products/ProductDetails/product-detail.component";
 import {WelcomeComponent} from "./welcome/welcome.component";
+import {ProductDetailsGuard} from "./products/ProductDetailsGuard/product-details.guard";
 
 export const AppRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
-  { path: 'products/:id', component: ProductDetailComponent },
+  { path: 'products/:id',
+    canActivate: [ ProductDetailsGuard ],
+    component: ProductDetailComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
   { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
