a126d5eea1074d9ee2a9096abfed52389011ff4f
lutfiqaraman <lutfiqaraman@gmail.com>
2022-01-11 22:01:33 +0200

    route page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {ProductListComponent} from "./products/ProductsList/product-list.compone
 import {ProductDetailComponent} from "./products/ProductDetails/product-detail.component";
 import {WelcomeComponent} from "./welcome/welcome.component";
 import {ProductDetailsGuard} from "./products/ProductDetailsGuard/product-details.guard";
+import {PagenotfoundComponent} from "./pagenotfound/pagenotfound.component";
 
 export const AppRoutes: Routes = [
   { path: 'products', component: ProductListComponent },
@@ -11,5 +12,6 @@ export const AppRoutes: Routes = [
     component: ProductDetailComponent },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+  { path: '404', component: PagenotfoundComponent },
+  { path: '**', redirectTo: '404', pathMatch: 'full' }
 ];
